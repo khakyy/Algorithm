406874d10d5cf5d8ba9259d19337ceb64d8df18c
HakYoon Kim <116627270+khakyy@users.noreply.github.com>
2024-02-07 22:05:21 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.nextLine();
+        int b = a.charAt(0);
+
+        System.out.println(b);
+    }
+}

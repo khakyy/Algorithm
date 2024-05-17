00a92b64209e6cf3737520550d8c374bc6d0dae0
HakYoon Kim <116627270+khakyy@users.noreply.github.com>
2024-05-17 19:30:59 +0900

    [Bronze V] Title: 연세대학교, Time: 208 ms, Memory: 17320 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        if (a == 0) {
+            System.out.println("YONSEI");
+        } else if (a == 1) {
+            System.out.println("Leading the Way to the Future");
+        }
+        
+    }
+}

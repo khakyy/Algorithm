44f5374fb3d89a44272297bba751548dfc38c499
HakYoon Kim <116627270+khakyy@users.noreply.github.com>
2024-05-29 19:48:08 +0900

    [Bronze V] Title: Rectangles, Time: 228 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

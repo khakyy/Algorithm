83ccf9c164c97851c19374382aaa52ed497e408a
HakYoon Kim <116627270+khakyy@users.noreply.github.com>
2024-06-14 19:09:30 +0900

    [Bronze V] Title: 이상한 기호, Time: 208 ms, Memory: 17716 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        
         System.out.println((a + b) * (a - b));
+        
     }
 }

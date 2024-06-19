f954a2931939cc4b8df972c5507b680bcc639aee
HakYoon Kim <116627270+khakyy@users.noreply.github.com>
2024-06-19 19:00:25 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 180 ms, Memory: 18288 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        System.out.println(":fan::fan::fan:\n" +
+                ":fan::"+ a +"::fan:\n" +
+                ":fan::fan::fan:");
+
+    }
+}

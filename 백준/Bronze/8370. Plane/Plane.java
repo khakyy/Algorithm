a2a451724801d044a80f1ed3e29e321a1af90235
HakYoon Kim <116627270+khakyy@users.noreply.github.com>
2024-06-29 19:46:11 +0900

    [Bronze V] Title: Plane, Time: 172 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b, c, d;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+        d = scanner.nextInt();
+
+        System.out.println((a * b) + (c * d));
+
+    }
+}
